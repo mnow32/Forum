571c2f9cb7b57bca7ec4891c581c9776d70edeba
mnowosielski <mnowosielski1917@gmail.com>
2026-02-22 20:16:50 +0100

    Added Post entity and applied initial migration to database

--- ForumAPI/Forum.API/Data/ForumDbContext.cs
+++ ForumAPI/Forum.API/Data/ForumDbContext.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Forum.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Forum.API.Data
 {
     public class ForumDbContext : DbContext
     {
+        public DbSet<Post> Posts { get; set; }
         public ForumDbContext(DbContextOptions<ForumDbContext> options) : base(options)
+        { 
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            base.OnModelCreating(modelBuilder);
+
         }
     }
 }
---
+++ ForumAPI/Forum.API/Entities/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Forum.API.Entities
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public required string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
