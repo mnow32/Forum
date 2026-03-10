727cf4fd29d1fd793d915242992dfcfcaac07f3e
mnowosielski <mnowosielski1917@gmail.com>
2026-03-10 23:46:02 +0100

    Added migration for RefreshTokenHash column

---
+++ ForumAPI/Forum.API/Data/Migrations/20260310224450_AddedRefreshTokenHashInForumUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forum.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRefreshTokenHashInForumUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RefreshToken",
+                table: "AspNetUsers",
+                newName: "RefreshTokenHash");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RefreshTokenHash",
+                table: "AspNetUsers",
+                newName: "RefreshToken");
+        }
+    }
+}
